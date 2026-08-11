b70c1def47914b4046cbd4842a4296757eee2a98
Max Charles Jr. <mcharles1682@gmail.com>
2026-08-10 21:49:25 -0400

    Make s3 script executable

--- s3_cleanup.sh
+++ s3_cleanup.sh
@@ -1,1 +1,9 @@
-aws s3 rm s3://<bucket> --recursive
+#!/bin/bash
+
+bucket_a="ingest-zone-storage-a-638039899567-us-east-1"
+bucket_b="ingest-zone-storage-b-638039899567-us-east-1"
+bucket_c="ingest-zone-storage-c-638039899567-us-east-1"
+
+aws s3 rm s3://${bucket_a} --recursive
+aws s3 rm s3://${bucket_b} --recursive
+aws s3 rm s3://${bucket_c} --recursive
